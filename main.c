f44b83e97757660e0fabd3325ae0aab29daaacf6
JuanCruz <juansantosdds13@gmail.com>
2024-03-09 01:58:30 -0300

    codigo de soma

--- main.c
+++ main.c
@@ -2,7 +2,17 @@
 
 int main (){
    
-    printf("ola mundo");
+    int num, num2, res;
+
+    printf("digite uma numero: ");
+    scanf("%d", &num);
+
+    printf("digite outro numero");
+    scanf("%d", &num2);
+
+    res = num + num2;
+
+    printf("o valor entre %d e %d e %d", num, num2, res);
 
     return 0; 
 
